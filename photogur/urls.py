db84d2154a2fb4d9957930eca5d1ce95e9bfb6a4
Ben Lardie <benlardie@gmail.com>
2019-03-06 10:51:45 -0500

    comments be working

--- photogur/urls.py
+++ photogur/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from photogur.views import picture_page, picture_show, root, picture_search
+from photogur.views import picture_page, picture_show, root, picture_search, create_comment
 
 urlpatterns = [
     path('', root),
@@ -23,4 +23,5 @@ urlpatterns = [
     path('pictures/', picture_page),
     path('pictures/<int:id>', picture_show, name='picture_details'),
     path('search', picture_search, name='picture_search'),
+    path('comments/new', create_comment, name='create_comment')
 ]
--- photogur/views.py
+++ photogur/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from .models import Picture
+from .models import Picture, Comment
 
 def root(request):
     return HttpResponseRedirect('pictures')
@@ -22,3 +22,12 @@ def picture_search(request):
     context = {'pictures': search_results, 'query': query}
     response = render(request, 'search.html', context)
     return HttpResponse(response)
+
+def create_comment(request):
+    picture = request.POST['picture']
+    comment = request.POST['new-comment']
+    new_comment = Comment.objects.create(name=request.POST['name'],
+                                         message=request.POST['new-comment'],
+                                         picture=Picture.objects.get(pk=request.POST['picture'])
+                                         )
+    return HttpResponseRedirect('/pictures/'+ request.POST['picture'])
