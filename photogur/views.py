a953749e7c43ffb472a3f262cd3b8464de587d6b
Ben Lardie <benlardie@gmail.com>
2019-03-11 15:51:44 -0400

    login works

--- photogur/views.py
+++ photogur/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .models import Picture, Comment
 from photogur.forms import LoginForm
+from django.contrib.auth import authenticate, login
 
 def root(request):
     return HttpResponseRedirect('pictures')
@@ -34,7 +35,21 @@ def create_comment(request):
     return HttpResponseRedirect('/pictures/'+ request.POST['picture'])
 
 def login_view(request):
-    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            pw = form.cleaned_data['password']
+            user = authenticate(username=username, password=pw)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/pictures/')
+            else:
+                form.add_error('username', 'Login failed')
+    else:
+        form = LoginForm()
+
+    # form = LoginForm()
     context = {'form': form}
     response = render(request, 'login.html', context)
     return HttpResponse(response)
