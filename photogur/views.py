d1296a028c2d13af0f06a4f572c2d8c380c64d96
Ben Lardie <benlardie@gmail.com>
2019-03-12 15:01:10 -0400

    cant access login or signup if already logge in

--- photogur/views.py
+++ photogur/views.py
@@ -45,6 +45,8 @@ def create_comment(request):
 
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/pictures')
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -69,6 +71,8 @@ def logout_view(request):
 
 
 def signup(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/pictures')
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
